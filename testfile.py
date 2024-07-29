0d61af77310312529a734e2b17132551fb5ca8fb
andreeatomescu16 <andreeatomescu040@gmail.com>
2024-07-29 10:55:12 +0300

    Create testfile.py

---
+++ testfile.py
@@ -0,0 +1,11 @@
+# Python program to display all the prime numbers within an interval
+
+lower = 10
+upper = 100
+
+for num in range(lower, upper + 1):
+       for i in range(3, num):
+           if (num % i) == 0:
+               break
+       else:
+           print(num)
